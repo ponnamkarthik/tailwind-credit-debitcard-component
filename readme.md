45879c6c654696c19b762d17062c260b0141fbba
Karthik Ponnam <ponnamkarthik3@gmail.com>
2021-02-24 19:29:53 +0530

    initial

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Credit/Debit Card Design
+
+## Demo
+
+https://tailwindcomponents.com/component/credit-debit-card/landing
+
+## Output
+
+![UI]()
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      letterSpacing: {
+        "more-wider": ".2rem"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
